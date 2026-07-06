e768bd6210fd458e166277844043e871a40718b8
RVESH <rishabhverma.rv2006@gmail.com>
2026-07-06 10:07:08 +0530

    makeing  universal_otp_

--- backend/src/providers/mongodb.provider.js
+++ backend/src/providers/mongodb.provider.js
@@ -1,31 +1,30 @@
+import { MongoClient } from "mongodb";
 
-let client = null;
+let clientPromise = null;
 let database = null;
 
 export async function getDatabase(env) {
-    console.log("MongoDB ENV:", {
-        hasEnv: !!env,
-        hasUri: !!env?.MONGODB_URI,
-        hasDatabase: !!env?.MONGODB_DATABASE,
-    });
-
-    if (database) {
-        return database;
-    }
-
-    if (!env.MONGODB_URI) {
-        throw new Error("MONGODB_URI is missing.");
-    }
-
-    if (!env.MONGODB_DATABASE) {
-        throw new Error("MONGODB_DATABASE is missing.");
-    }
-
-    client = new MongoClient(env.MONGODB_URI);
-
-    await client.connect();
-
-    database = client.db(env.MONGODB_DATABASE);
-
+  if (database) {
     return database;
+  }
+
+  if (!env?.MONGODB_URI) {
+    throw new Error("MONGODB_URI is missing.");
+  }
+
+  if (!env?.MONGODB_DATABASE) {
+    throw new Error("MONGODB_DATABASE is missing.");
+  }
+
+  if (!clientPromise) {
+    const client = new MongoClient(env.MONGODB_URI);
+
+    clientPromise = client.connect();
+  }
+
+  const client = await clientPromise;
+
+  database = client.db(env.MONGODB_DATABASE);
+
+  return database;
 }
