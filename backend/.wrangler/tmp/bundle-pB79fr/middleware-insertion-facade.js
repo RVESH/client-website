af95ba091aa2f7ff4577441d5092824ad08a4820
RVESH <rishabhverma.rv2006@gmail.com>
2026-07-08 20:35:12 +0530

    debugging

---
+++ backend/.wrangler/tmp/bundle-pB79fr/middleware-insertion-facade.js
@@ -0,0 +1,10 @@
+				import worker, * as OTHER_EXPORTS from "G:\\apage\\one\\backend\\src\\worker.js";
+				import * as __MIDDLEWARE_0__ from "C:\\Users\\rishabhverma.rv2006\\AppData\\Roaming\\npm\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
+
+				export * from "G:\\apage\\one\\backend\\src\\worker.js";
+				const MIDDLEWARE_TEST_INJECT = "__INJECT_FOR_TESTING_WRANGLER_MIDDLEWARE__";
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default
+				]
+				export default worker;
